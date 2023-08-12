6381c65a4e69756dc96ef3e4ac8b8cc62dcbdb67
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-08-12 17:57:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# commconnect
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/thejitenpatel/commconnect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thejitenpatel.commconnect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
